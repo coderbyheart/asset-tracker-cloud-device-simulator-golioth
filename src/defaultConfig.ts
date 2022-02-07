f8202bd25a078aec6331aae57c157a047e59aa5f
Markus Tacker <Markus.Tacker@NordicSemi.no>
2022-02-07 13:47:06 +0100

    fix: use GNSS instead of GPS

--- src/defaultConfig.ts
+++ src/defaultConfig.ts
@@ -3,7 +3,7 @@ export const defaultConfig = {
 	actwt: 60, //In active mode: wait this amount of seconds until sending the next update. The actual interval will be this time plus the time it takes to get a GPS fix.
 	mvres: 300, // (movement resolution) In passive mode: Time in seconds to wait after detecting movement before sending the next update
 	mvt: 3600, // (movement timeout) In passive mode: Send update at least this often (in seconds)
-	gpst: 60, // GPS timeout (in seconds): timeout for GPS fix
+	gnsst: 60, // GNSS timeout (in seconds): timeout for GNSS fix
 	acct: 0.1, // Accelerometer threshold: minimal absolute value for and accelerometer reading to be considered movement.
 	/**
 	 * List of modules which should be disabled when sampling data.
